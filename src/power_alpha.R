9aa35cb9b69b682384d9b5583e28173de9d15006
whopper-cemnl <wjhopper510@gmail.com>
2015-01-06 16:50:33 -0500

    ggplot-ized all the animations, yay

--- src/power_alpha.R
+++ src/power_alpha.R
@@ -1,49 +1,95 @@
-power_alpha <- function(meanh0 = 100, effect =3, N = 15, sdh0 = 15, interval = 2.5, frames = 10) {
-  
+power_alpha <- function(meanh0 = 100, effect =5, N = 25, sdh0 = 15, interval = 2.5, frames = 10) {
+  library(ggplot2)
+  library(animation)
   # POWER GOES DOWN AS ALPHA, THE SIGNIFICANCE LEVEL, DECREASES
   oopt = ani.options()
   ani.options(interval = interval, nmax = frames)
   
-  ## use a loop to create images one by one
-  for (alpha in ani.options("nmax"):1) {
-    alpha = alpha/100;
-    cutoff = qnorm(1-alpha)
-    xv<-seq(meanh0 - 20*sdh0/sqrt(N),meanh0 + 20*sdh0/sqrt(N),0.01)  # We create a vector of x values over the range of values
-    yv<-dnorm(xv,0,1)  # We create the corresponding vector of normal densities
-    curve(dnorm(x, meanh0, sdh0/sqrt(N)), from =  meanh0 - 10*sdh0/sqrt(N), to = meanh0 + 10*sdh0/sqrt(N), 0.01, n = 200, ylim = c(0,.15));  # We draw the normal curve
+  se <- sdh0/sqrt(N)
+  meanh1 <- meanh0+effect
+  
+  ## use a loop to create images one by one over some nice alpha levels
+  for (alpha in seq(.2,.01, by=-.2/frames)) {
+    cutoff = qnorm(1-alpha,meanh0,se)
+    power <- (power.t.test(n=N,delta = effect, sd = sdh0,sig.level = alpha, 
+                          type = "one.sample", alternative = "one.sided")$power)
+    Beta <- round((1-power),2)
     
-    power = round( pnorm(meanh0 - cutoff*sdh0/sqrt(N), meanh0 + effect, sdh0/sqrt(N)) + 1 - pnorm(meanh0 + cutoff*sdh0/sqrt(N), meanh0 + effect, sdh0/sqrt(N)),3)
-    text(meanh0 + effect + 5*sdh0/sqrt(N), .10, paste("alpha = ", as.character(alpha)))
-    text(meanh0 + effect + 5*sdh0/sqrt(N), .08, paste("Power = ", as.character(power)))
+    # We create a vector of x values over the range of values, stick in data frame
+    df <- data.frame(xv = seq(meanh0 - 4*se, meanh1 + 4*se, 0.01))
+    df$yv_null <-dnorm(df$xv,meanh0,se)
+    df$yv_alt <-dnorm(df$xv,meanh1,se)
     
-    polygon(      		# We draw the left tail alpha/2
-    c(meanh0 - 6*sdh0/sqrt(N), 
-    xv[(xv<=meanh0 - cutoff*sdh0/sqrt(N))&(xv>=meanh0 - 6*sdh0/sqrt(N))],  
-    meanh0 - cutoff*sdh0/sqrt(N) ), 
-    c(0,  dnorm(xv[(xv<=meanh0 - cutoff*sdh0/sqrt(N))&(xv>=meanh0 - 6*sdh0/sqrt(N))] ,meanh0,sdh0/sqrt(N)),  0) , col="red")  
-    polygon(			# We draw the right tail alpha/2
-    c(meanh0 + cutoff*sdh0/sqrt(N), 
-    xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + 6*sdh0/sqrt(N))],  
-    meanh0 + 6*sdh0/sqrt(N) ), 
-    c(0,  dnorm(xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + 6*sdh0/sqrt(N))] ,meanh0,sdh0/sqrt(N)),  0) , col="red")  
-    
-    polygon(			# We draw the left tail power
-    c(meanh0 - 6*sdh0/sqrt(N), 
-    xv[(xv>=meanh0 - 6*sdh0/sqrt(N))&(xv<=meanh0 - cutoff*sdh0/sqrt(N))],  
-    meanh0 - cutoff*sdh0/sqrt(N) ), 
-    c(0,  dnorm(xv[(xv>=meanh0 - 6*sdh0/sqrt(N))&(xv<=meanh0 - cutoff*sdh0/sqrt(N))] ,meanh0+ effect,sdh0/sqrt(N)),  0) , col=rgb(0,0,1,.5))  
-    
-    polygon(			# We draw the right tail power
-    c(meanh0 + cutoff*sdh0/sqrt(N), 
-    xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + effect + 20*sdh0/sqrt(N))],  
-    meanh0+ effect + 20*sdh0/sqrt(N) ), 
-    c(0,  dnorm(xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + effect + 20*sdh0/sqrt(N))], meanh0+ effect,sdh0/sqrt(N)),  0) , col=rgb(0,0,1,.5))  
-    
-    polygon(			# We draw the beta
-    c(meanh0 - cutoff*sdh0/sqrt(N), 
-    xv[(xv<=meanh0 + cutoff*sdh0/sqrt(N))&(xv>=meanh0  - cutoff*sdh0/sqrt(N))],  
-    meanh0+ cutoff*sdh0/sqrt(N) ), 
-    c(0,  dnorm(xv[(xv<=meanh0 + cutoff*sdh0/sqrt(N))&(xv>=meanh0  - cutoff*sdh0/sqrt(N))], meanh0+ effect,sdh0/sqrt(N)),  0) , col=rgb(0,1,0,.5))  
+
+    p1 <- ggplot(data =df, aes(x=xv)) + 
+      geom_line(aes(y=yv_null),size=1) +
+      geom_line(aes(y=yv_alt),size=1) +
+      # Beta 
+      geom_polygon(data = data.frame(x = c(min(df$xv), df[df$xv <= cutoff,'xv'], cutoff),
+                                     y = c(0, df[df$xv <= cutoff,'yv_alt'], 0)),
+                   aes(x=x,y=y), fill='blue',color='black',alpha = .5) + 
+      # power
+      geom_polygon(data = data.frame(x = c(cutoff, df[df$xv >= cutoff,'xv'], max(df$xv)),
+                                     y = c(0, df[df$xv >= cutoff,'yv_alt'], 0)),
+                   aes(x=x,y=y), fill='green',color='black',alpha = .5) + 
+      # alpha
+      geom_polygon(data = data.frame(x = c(cutoff, df[df$xv >= cutoff,'xv'], max(df$xv)),
+                                     y = c(0, df[df$xv >= cutoff,'yv_null'], 0)),
+                   aes(x=x,y=y), fill='red',color='black',alpha=.6) + 
+      geom_text(data=data.frame( x = cutoff+2, y = .101*max(df$yv_null), alpha = alpha), 
+                mapping = aes(label = paste("alpha ==",alpha), x=x, y=y),
+                parse=TRUE, size = 6) + 
+      geom_text(data=data.frame( x = cutoff-2, y = .1*max(df$yv_null), Beta = Beta), 
+                mapping = aes(label = paste("beta ==",Beta), x=x, y=y),
+                parse=TRUE, size = 6) +
+      geom_text(data=data.frame( x = max(meanh0,cutoff+3), y = .5*max(df$yv_null), power=power), 
+                mapping = aes(label = paste("power ==",round(power,2)), x=x, y=y),
+                parse=TRUE, size = 6) + 
+      geom_text(data=data.frame( x = meanh0, y = max(df$yv_null)+.01),
+                mapping = aes(label = "H[0]", x=x, y=y), parse=TRUE, size = 8) + 
+      geom_text(data=data.frame( x = meanh1, y = max(df$yv_alt)+.01),
+                mapping = aes(label = "H[1]", x=x, y=y), parse=TRUE, size = 8) + 
+      scale_x_continuous("Numeric Dependent Variable Values") + 
+      scale_y_continuous("Normal Distribution") +
+      ggtitle("Power as a function of alpha level") + theme_bw() +
+      theme(plot.title = element_text(size=18),
+            panel.grid.major = element_blank())
+    print(p1)
+ 
+ 
+    #     yv<-dnorm(xv,0,1)  # We create the corresponding vector of normal densities
+    #     curve(dnorm(x, meanh0, sdh0/sqrt(N)), from =  meanh0 - 10*sdh0/sqrt(N), to = meanh0 + 10*sdh0/sqrt(N), 0.01, n = 200, ylim = c(0,.15));  # We draw the normal curve
+    #     power = round( pnorm(meanh0 - cutoff*se, meanh0 + effect, sdh0/sqrt(N)) + 1 - pnorm(meanh0 + cutoff*sdh0/sqrt(N), meanh0 + effect, sdh0/sqrt(N)),3)
+    #     text(meanh0 + effect + 5*sdh0/sqrt(N), .10, paste("alpha = ", as.character(alpha)))
+    #     text(meanh0 + effect + 5*sdh0/sqrt(N), .08, paste("Power = ", as.character(power)))  
+    #     polygon(      		# We draw the left tail alpha/2
+    #     c(meanh0 - 6*sdh0/sqrt(N), 
+    #     xv[(xv<=meanh0 - cutoff*sdh0/sqrt(N))&(xv>=meanh0 - 6*sdh0/sqrt(N))],  
+    #     meanh0 - cutoff*sdh0/sqrt(N) ), 
+    #     c(0,  dnorm(xv[(xv<=meanh0 - cutoff*sdh0/sqrt(N))&(xv>=meanh0 - 6*sdh0/sqrt(N))] ,meanh0,sdh0/sqrt(N)),  0) , col="red")  
+    #     polygon(			# We draw the right tail alpha/2
+    #     c(meanh0 + cutoff*sdh0/sqrt(N), 
+    #     xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + 6*sdh0/sqrt(N))],  
+    #     meanh0 + 6*sdh0/sqrt(N) ), 
+    #     c(0,  dnorm(xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + 6*sdh0/sqrt(N))] ,meanh0,sdh0/sqrt(N)),  0) , col="red")  
+    #     
+    #     polygon(			# We draw the left tail power
+    #     c(meanh0 - 6*sdh0/sqrt(N), 
+    #     xv[(xv>=meanh0 - 6*sdh0/sqrt(N))&(xv<=meanh0 - cutoff*sdh0/sqrt(N))],  
+    #     meanh0 - cutoff*sdh0/sqrt(N) ), 
+    #     c(0,  dnorm(xv[(xv>=meanh0 - 6*sdh0/sqrt(N))&(xv<=meanh0 - cutoff*sdh0/sqrt(N))] ,meanh0+ effect,sdh0/sqrt(N)),  0) , col=rgb(0,0,1,.5))  
+    #     
+    #     polygon(			# We draw the right tail power
+    #     c(meanh0 + cutoff*sdh0/sqrt(N), 
+    #     xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + effect + 20*sdh0/sqrt(N))],  
+    #     meanh0+ effect + 20*sdh0/sqrt(N) ), 
+    #     c(0,  dnorm(xv[(xv>=meanh0 + cutoff*sdh0/sqrt(N))&(xv<=meanh0 + effect + 20*sdh0/sqrt(N))], meanh0+ effect,sdh0/sqrt(N)),  0) , col=rgb(0,0,1,.5))  
+    #     
+    #     polygon(			# We draw the beta
+    #     c(meanh0 - cutoff*sdh0/sqrt(N), 
+    #     xv[(xv<=meanh0 + cutoff*sdh0/sqrt(N))&(xv>=meanh0  - cutoff*sdh0/sqrt(N))],  
+    #     meanh0+ cutoff*sdh0/sqrt(N) ), 
+    #     c(0,  dnorm(xv[(xv<=meanh0 + cutoff*sdh0/sqrt(N))&(xv>=meanh0  - cutoff*sdh0/sqrt(N))], meanh0+ effect,sdh0/sqrt(N)),  0) , col=rgb(0,1,0,.5))  
   }		
   	
    }
--- src/regression_outliers.R
+++ src/regression_outliers.R
@@ -1,41 +1,84 @@
-regression_outliers <- function(n = 50, mu = 100, sigma = 15, interval = 2.5, frames =20) { 
+regression_outliers <- function(n = 51, mu = 100, sigma = 15, interval = 2.5, frames =20) { 
 
-  ani.options(interval = interval, outlier.slope.max = frames)
+  ani.options(interval = interval, nmax = frames)
 
   #1 The effect of an outlier on the regression line when the outlier is far from the mean.
   x = rnorm(n, mu, sigma)
-  y = 3*x + 5 + rnorm(n)
-  outx = 130
-  plot(x,y,  xlim =c(70, 140),ylim = c(100,900), type = "n")
-  abline(lm(y~x), col = "red")
+  y = .66*x + 5 + rnorm(n,0,5)
+  df <- data.frame(xv = x, yv = y)
+  max_x <- max(df$xv)
+  median_x <- median(df$x)
+  max_y <- max(df$yv) + frames
   
-  for (outlier.slope in 3:ani.options("outlier.slope.max")) {
-    ol.slope = 2.8 + outlier.slope/5	
-    outy =  ol.slope*outx + 5
-    xplot = c(x, outx)
-    yplot = c(y, outy)
-    plot(x,y,  xlim = c(min(xplot), max(xplot)),ylim = c(100,900))
-    abline(lm(y~x), col = "red")
-    points(xplot,yplot, ylim = c(100,900), xlim = c(min(xplot), max(xplot)))
-    abline(lm(yplot ~ xplot))	
-  }
+    
+#   plot(x,y,  xlim =c(70, 140),ylim = c(100,900), type = "n")
+#   abline(lm(y~x), col = "red")
   
-  #2 The effect of an outlier on the regression line when the outlier is not far from the mean.
-  x = rnorm(n, mu, sigma)
-  y = 3*x + 5 + rnorm(n)
-  outx = 101
-  plot(x,y,  xlim =c(70, 140),ylim = c(100,900), type = "n")
-  abline(lm(y~x), col = "red")
+  for (residual in seq(2,2+frames)) {
+      mod_data <- df
+      mod_data_median <- df
+      mod_data[mod_data$xv ==max_x,'yv'] <- mod_data[mod_data$xv ==max_x,'yv'] + residual
+#       outlier = mod_data[mod_data$xv==max_x,]
+      mod_data_median[mod_data_median$xv ==median_x,'yv'] <- mod_data_median[mod_data_median$xv ==median_x,'yv'] + residual
+      lm.normal <- lm(yv~xv,data=df)
+      lm.far <- lm(yv~xv,data=mod_data)
+      lm.near <- lm(yv~xv,data=mod_data_median)
+      delta_far <- lm.far$coefficients["xv"] - lm.normal$coefficients["xv"]
+      delta_near <- lm.near$coefficients["xv"] - lm.normal$coefficients["xv"]
 
-  for (outlier.slope in 3:ani.options("outlier.slope.max")) {
-    ol.slope = 2.8 + outlier.slope/5	
-    outy =  ol.slope*outx + 5
-    xplot = c(x, outx)
-    yplot = c(y, outy)
-    plot(x,y,  xlim = c(min(xplot), max(xplot)),ylim = c(100,900))
-    abline(lm(y~x), col = "red")
-    points(xplot,yplot, ylim = c(100,900), xlim = c(min(xplot), max(xplot)))
-    abline(lm(yplot ~ xplot))	
+      p <- ggplot(data=df, mapping = aes(x=xv,y=yv)) + 
+        geom_point(size=4) +
+        geom_smooth(method="lm", se=F,color='black',size = 1.25) + 
+          scale_x_continuous("Numeric Depedent Variable  #1") +
+          scale_y_continuous("Numeric Depedent Variable  #2", limits=c(min(df$yv), max_y)) +
+          theme_bw() + 
+          theme(plot.title = element_text(size=18),
+                panel.grid.major = element_blank())
+
+      p1 <- p + geom_smooth(data=mod_data,mapping=aes(x=xv,y=yv),
+                            method="lm",se=F,color='red',size = 1.25) +
+            geom_point(data= mod_data[mod_data$xv==max_x,],
+                       mapping=aes(x=xv, y=yv),color='red',size=4) +
+            geom_text(data = data.frame(x=mean(mod_data$xv), y  =100,d = round(delta_far,3)),
+                      mapping=aes(label = paste("Change in Slope =",d),x=x,y=y), size=6) + 
+            ggtitle("Outlier far from Mean of X")  
+
+      p2 <- p + geom_smooth(data=mod_data_median,mapping=aes(x=xv,y=yv),
+                         method="lm",se=F,color='red',size = 1.25) +
+            geom_point(data= mod_data_median[mod_data_median$xv==median_x,],
+                       mapping=aes(x=xv, y=yv),color='red',size=4) + 
+            geom_text(data = data.frame(x=mean(mod_data_median$xv), y  =100, d = round(delta_near,3)),
+                      mapping=aes(label = paste("Change in Slope =",d),x=x,y=y), size=6) + 
+            theme(axis.title.y= element_blank()) + 
+            ggtitle("Outlier near Mean of X")
+
+      grid.arrange(p1,p2,ncol=2,nrow=1)
   }
-	
+
+#     ol.slope = 2.8 + outlier.slope/5  
+#     outy =  ol.slope*outx + 5
+#     xplot = c(x, outx)
+#     yplot = c(y, outy)
+#     plot(x,y,  xlim = c(min(xplot), max(xplot)),ylim = c(100,900))
+#     abline(lm(y~x), col = "red")
+#     points(xplot,yplot, ylim = c(100,900), xlim = c(min(xplot), max(xplot)))
+#     abline(lm(yplot ~ xplot))	
+#   }
+#   
+#   #2 The effect of an outlier on the regression line when the outlier is not far from the mean.
+#   x = rnorm(n, mu, sigma)
+#   y = 3*x + 5 + rnorm(n)
+#   outx = 101
+#   plot(x,y,  xlim =c(70, 140),ylim = c(100,900), type = "n")
+#   abline(lm(y~x), col = "red")
+#   
+#   for (outlier.slope in 3:ani.options("outlier.slope.max")) {
+#     ol.slope = 2.8 + outlier.slope/5	
+#     outy =  ol.slope*outx + 5
+#     xplot = c(x, outx)
+#     yplot = c(y, outy)
+#     plot(x,y,  xlim = c(min(xplot), max(xplot)),ylim = c(100,900))
+#     abline(lm(y~x), col = "red")
+#     points(xplot,yplot, ylim = c(100,900), xlim = c(min(xplot), max(xplot)))
+#     abline(lm(yplot ~ xplot))	
 }
