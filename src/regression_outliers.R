e52586bb2d28c4d6898ad1d47650fa6cd853794c
whopper-sculder <wjhopper510@gmail.com>
2015-01-08 12:42:19 -0500

    Changed some theme/size stuff to be smaller because fig.width and fig.height seem to get doubled when using grid.arrange (seems the options get applied to each gg object, not final result).

--- src/regression_outliers.R
+++ src/regression_outliers.R
@@ -1,17 +1,13 @@
-regression_outliers <- function(n = 50, mu = 100, sigma = 15, interval = .05, frames =20) { 
+regression_outliers <- function(n = 35, range=c(70,130), interval = .75, frames =20) { 
   library(ggplot2)
   library(gridExtra)
   ani.options(interval = interval, nmax = frames + 1)
   offset = 1 
-
   n = round(n)
-#   if (n %% 2 != 0) {
-#     stop("Number of observations must be an even integer")
-#   }
   
   #1 The effect of an outlier on the regression line when the outlier is far from the mean.
-  x = rnorm(n, mu, sigma)
-  y = .4*x + 5 + rnorm(n,0,5)
+  x = runif(n, min=range[1], max=range[2])
+  y = .3*x + 3.5 + rnorm(n,0,3)
   # not really random, but make sure most extreme x has most extreme y =)
   max_x <- max(x)
   y[x==max_x] <- max_y <- max(y) + offset
@@ -30,8 +26,8 @@ regression_outliers <- function(n = 50, mu = 100, sigma = 15, interval = .05, fr
   # Get some good lims
   min_ylim <- min(y) - (ani.options("nmax") - (abs((min(y) - y[middle_x_pos]))))
   max_ylim <- max(y) + ani.options("nmax")
-  min_xlim <- min(x) - 5
-  max_xlim <- max(x)+ 5
+  min_xlim <- min(x) - 1
+  max_xlim <- max(x)+ 1
 
   for (residual in seq(offset-1,offset-1+frames)) {
 
@@ -49,27 +45,30 @@ regression_outliers <- function(n = 50, mu = 100, sigma = 15, interval = .05, fr
     delta_near <- lm.near$coefficients["xv"] - lm.normal$coefficients["xv"]
 
     p <- ggplot(data=df, mapping = aes(x=xv,y=yv)) + 
-      geom_point(size=4) +
-      geom_smooth(method="lm", se=F,color='black',size = 1.25) + 
+      geom_point(size=1.5) +
+      geom_smooth(method="lm", se=F,color='black') + 
       scale_x_continuous("Numeric Depedent Variable  #1", limits=c(min_xlim, max_xlim)) +
       scale_y_continuous("Numeric Depedent Variable  #2", limits=c(min_ylim, max_ylim)) +
       theme_bw() + 
-      theme(plot.title = element_text(size=18),
-            panel.grid.major = element_blank())
+      theme(plot.title = element_text(size=10),
+            panel.grid.major = element_blank(),
+            axis.title.y = element_text(size=8),
+            axis.title.x = element_text(size=8),
+            plot.margin = unit(c(0,.25,.25,.25),units="lines"))
 
     p1 <- p + geom_smooth(data=m_df,mapping=aes(x=xv,y=yv),
-                          method="lm",se=F,color='red',size = 1.25) +
+                          method="lm",se=F,color='red') +
           geom_point(data= m_df[m_df$xv==max_x,],
-                     mapping=aes(x=xv, y=yv),color='red',size=4) +
+                     mapping=aes(x=xv, y=yv),color='red',size=1.5) +
           geom_text(data = data.frame(x=mean(m_df$xv), y  = max_ylim -5  , d = round(delta_far,3)),
-                    mapping=aes(label = paste("Change in Slope =",d),x=x,y=y), size=6) + 
+                    mapping=aes(label = paste("Change in Slope =",d),x=x,y=y), size=3) + 
           ggtitle("Outlier far from Mean of X")  
     p2 <- p + geom_smooth(data=m_df_mid,mapping=aes(x=xv,y=yv),
-                          method="lm",se=F,color='red',size = 1.25) +
+                          method="lm",se=F,color='red') +
           geom_point(data= m_df_mid[middle_x_pos,],
-                     mapping=aes(x=xv, y=yv),color='red',size=4) + 
+                     mapping=aes(x=xv, y=yv),color='red',size=1.5) + 
           geom_text(data = data.frame(x=mean(m_df_mid$xv), y  = max_ylim -5, d = round(delta_near,3)),
-                    mapping=aes(label = paste("Change in Slope =",d),x=x,y=y), size=6) + 
+                    mapping=aes(label = paste("Change in Slope =",d),x=x,y=y), size=3) + 
           theme(axis.title.y= element_blank()) + 
           ggtitle("Outlier near Mean of X")
 
