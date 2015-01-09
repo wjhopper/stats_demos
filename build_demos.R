ec06ac763c5ecdaa3d6e89e3b90499fe1eb70f20
whopper-cemnl <wjhopper510@gmail.com>
2015-01-09 15:41:14 -0500

    first draft of having a single resuable plot-potting function set up instead of duplicating 95% of plotting code over 4 different functions. Also, build_demos() doesn't rely on looking for files to build the function table

--- build_demos.R
+++ build_demos.R
@@ -1,38 +1,46 @@
-build_demos <- function(docs="all",anim_only = FALSE, anim_dir = "anim_output", html_dir = "html_output") { 
+build_demos <- function(docs=c("power_alpha","power_n", "power_sd", "power_effect", "regression_outliers",
+                               "sampling_distribution"), param_set=NULL, anim_only = FALSE, anim_dir = "anim_output", html_dir = "html_output") { 
   library(animation)
+  library(ggplot2)
+  library(gridExtra)
+  start_dir = getwd()
   
-  files <- list.files(path = "src",pattern="*.R",full.names=TRUE)
-  if (identical(files, character(0))) {
-    stop("No animation functions found. Perhaps you are in the wrong directory?
-  Change to the directory containing the 'src' directory and try again.")
-  }
-  
-  if (docs != "all") {
-    files = files[sub('.R', '', basename(files)) == docs]
-  }
-  bnames <- sub('.R','',basename(files))
+  # function table to look up right function call for demo
+  fun_table <- list(power_alpha = list(name="power"),power_n = list(name="power"), power_sd=list(name = "power"),
+                    power_effect= list(name="power"), regression_outliers =list(name="regression_outliers"),
+                    sampling_distribution = list(name="sampling_distribution"))
 
+  args_list <- list(power_alpha = list(meanh0 = 100, sdh0 = 15, effect =5, N = 25, interval = 2.5, frames = 10),
+       power_effect = list(meanh0 = 100, sdh0 = 15, alpha=.05,  N =25, interval=2.5, frames = 10),
+       power_n = list(meanh0 = 100, sdh0 = 15, effect = 3,  alpha=.05 , interval=2.5, frames = 30),
+       power_sd = list(meanh0 = 100, effect = 3,  alpha=.05 , N =25, interval=2.5, frames = 30),
+       regression_outliers = list(n = 20, range=c(70,130), interval = .75, frames =20),
+       sampling_Distribution = list(samps = 10, n = 50, rate = .03)
+      )
+  # prune table based on input args
+  fun_table <- fun_table[docs]
   
-  if (anim_only) {
-    files <- as.list(files)
-    names(files) <- bnames
-    fun_list <- lapply(files,source)
-    
-    start_dir = getwd()
+  # add the file path and arguments on there 
+  for (i in 1:length(fun_table)) { 
+#     fun_table[[i]]$name <- file.path("src",fun_table[[i]]$name)
+    fun_table[[i]]$fun <- source(file.path("src",paste(fun_table[[i]]$name,'.R',sep=''))) # to do = throw custom error about changing directory
+    fun_table[[i]]$args <- args_list[names(fun_table[i])][[1]]
+  }
+
+  if (anim_only) {    
     dir.create(anim_dir, mode = "0775")
-    for (i in 1:length(fun_list)) {
+    for (i in 1:length(fun_table)) {
       setwd("anim_output")
-      saveHTML({fun_list[[i]]$value()}, img.name = bnames[i], 
-               imgdir =  "img",  htmlfile = paste(bnames[i],".html", sep=''),
+      saveHTML({do.call(fun_table[[i]]$fun$value,fun_table[[i]]$args)}, img.name = fun_table[[i]]$name, 
+               imgdir =  "img",  htmlfile = paste(fun_table[[i]]$name,".html", sep=''),
                autobrowse = FALSE, verbose=FALSE, autoplay =FALSE, navigator = FALSE,
-               title = bnames[i],ani.width = 900, ani.height=500)
+               title = fun_table[[i]]$name,ani.width = 900, ani.height=500)
       setwd(start_dir)
     }
   } else {
       library(rmarkdown)
       library(knitr)
-      library(ggplot2)
-      library(gridExtra)
+
       
       opts_knit$set(animation.fun=hook_scianimator)
       start_dir = getwd()
---
+++ src/power.R
@@ -0,0 +1,88 @@
+power<- function(meanh0 = 100, effect =NULL, N = NULL, sdh0 = NULL, alpha = NULL, interval = 2.5, frames = 10) {
+
+  draw_plot <- function() { 
+    se <- sdh0/sqrt(N)
+    meanh1 <- meanh0+effect
+    cutoff = qnorm(1-alpha,meanh0,se)
+    power <- (power.t.test(n=N,delta = effect, sd = sdh0, sig.level = alpha, 
+                           type = "one.sample", alternative = "one.sided")$power)
+    Beta <- round((1-power),2)
+    
+    # We create a vector of x values over the range of values, stick in data frame
+    df <- data.frame(xv = seq(meanh0 - 4*se, meanh1 + 4*se, 0.01))
+    df$yv_null <-dnorm(df$xv,meanh0,se)
+    df$yv_alt <-dnorm(df$xv,meanh1,se)
+    
+    
+    p1 <- ggplot(data =df, aes(x=xv)) + 
+      geom_line(aes(y=yv_null)) +
+      geom_line(aes(y=yv_alt)) +
+      # Beta 
+      geom_polygon(data = data.frame(x = c(min(df$xv), df[df$xv <= cutoff,'xv'], cutoff),
+                                     y = c(0, df[df$xv <= cutoff,'yv_alt'], 0)),
+                   aes(x=x,y=y), fill='blue',color='black',alpha = .5) + 
+      # power
+      geom_polygon(data = data.frame(x = c(cutoff, df[df$xv >= cutoff,'xv'], max(df$xv)),
+                                     y = c(0, df[df$xv >= cutoff,'yv_alt'], 0)),
+                   aes(x=x,y=y), fill='green',color='black',alpha = .5) + 
+      # alpha
+      geom_polygon(data = data.frame(x = c(cutoff, df[df$xv >= cutoff,'xv'], max(df$xv)),
+                                     y = c(0, df[df$xv >= cutoff,'yv_null'], 0)),
+                   aes(x=x,y=y), fill='red',color='black',alpha=.6) + 
+      geom_text(data=data.frame( x = cutoff+2, y = .101*max(df$yv_null), alpha = alpha), 
+                mapping = aes(label = paste("alpha ==",alpha), x=x, y=y),
+                parse=TRUE, size = 3) + 
+      geom_text(data=data.frame( x = cutoff-2, y = .1*max(df$yv_null), Beta = Beta), 
+                mapping = aes(label = paste("beta ==",Beta), x=x, y=y),
+                parse=TRUE, size = 3) +
+      geom_text(data=data.frame( x = max(meanh0,cutoff+3), y = .5*max(df$yv_null), power=power), 
+                mapping = aes(label = paste("power ==",round(power,2)), x=x, y=y),
+                parse=TRUE, size = 3) + 
+      geom_text(data=data.frame( x = meanh0, y = max(df$yv_null)+.01),
+                mapping = aes(label = "H[0]", x=x, y=y), parse=TRUE, size = 4) + 
+      geom_text(data=data.frame( x = meanh1, y = max(df$yv_alt)+.01),
+                mapping = aes(label = "H[1]", x=x, y=y), parse=TRUE, size = 4) + 
+      scale_x_continuous("Numeric Dependent Variable Values") + 
+      scale_y_continuous("Normal Distribution") +
+      ggtitle("Power as a function of alpha level") + theme_bw() +
+      theme(plot.title = element_text(size=10),
+            panel.grid.major = element_blank(),
+            axis.title.y = element_text(size=8),
+            axis.title.x = element_text(size=8),
+            plot.margin = unit(c(0,.25,.25,.25),units="lines"))
+    print(p1)
+  
+  }
+
+  args_list <- list(N, effect, sdh0, alpha)
+  if (sum(sapply(list(N, effect, sdh0, alpha), is.null)) != 1) { 
+    stop("exactly one of 'n', 'effect', 'sdh0', and 'alpha' must be NULL")
+  }
+  
+  if (!is.null(alpha) && !is.numeric(alpha) || any(0 > alpha | alpha > 1)) { 
+    stop("'sig.level' must be numeric in [0, 1]")
+  }
+  
+  opts <- ani.options()
+  ani.options(interval = interval, nmax = frames)
+  
+  if (is.null(N)) {
+#     N <- seq(5,frames,by = 5)
+    for (N in seq(5,frames,by = 5)) {
+      draw_plot() 
+    } 
+  } else if (is.null(sdh0))  { 
+    sd <- seq(15,1,length.out=frames)
+    var_int <- sd
+  
+  } else if (is.null(effect)) { 
+    effect <- seq(1,frames,by=1)
+    var_int <- effect
+
+  } else if (is.null(alpha)) { 
+    alpha = seq(.2,.01, by=-.2/frames)
+    var_int <- alpha
+  } else { 
+    stop("internal error herp")
+  }
+}
